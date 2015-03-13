085490949102f7ed68f91fb826ae14a022c5f67c
keaganr <keaganr@gmail.com>
2015-03-13 15:51:04 -0400

    added basic expand animation to height page

--- FoTaITO/src/com/mtu/ito/fotaito/frontend/SavedListingsActivity.java
+++ FoTaITO/src/com/mtu/ito/fotaito/frontend/SavedListingsActivity.java
@@ -13,7 +13,10 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -87,12 +90,12 @@ public class SavedListingsActivity extends MyActivity {
 
 
                 // gather screen information. Used in conversion from dp -> pixels
-                DisplayMetrics metrics = getResources().getDisplayMetrics();
+                final DisplayMetrics metrics = getResources().getDisplayMetrics();
 
                 // variables for displaying blocks
                 int marginTop = (int) (metrics.density * 10f + 0.5f); // convert dp to pixels
                 int marginBottom = (int) (metrics.density * 10f + 0.5f);
-                float blockHeight = 75f;    // block size
+                final int blockHeight = (int) (metrics.density * 75f + 0.5f);    // block size
                 boolean color = true;       // swapper for alternating color
                 int lastViewID = 0;         // holds a reference to the previous blocks id
 
@@ -102,17 +105,22 @@ public class SavedListingsActivity extends MyActivity {
                     // set relative layout (block) parameters
                     RelativeLayout new_listing_layout = new RelativeLayout(SavedListingsActivity.this);
                     RelativeLayout.LayoutParams rlp = new RelativeLayout.LayoutParams(
-                            RelativeLayout.LayoutParams.MATCH_PARENT,
-                            (int) (metrics.density * blockHeight + 0.5f)); // convert blockHeight from dp to pixels
-
-                    // set bottom margin
-                    rlp.bottomMargin = marginBottom;
+                            RelativeLayout.LayoutParams.MATCH_PARENT, blockHeight);
+                    rlp.bottomMargin = marginBottom; // set bottom margin
 
                     // set top align. marginTop if first, below last if not
                     if (lastViewID == 0)
                         rlp.topMargin = marginTop;
                     else
                         rlp.addRule(RelativeLayout.BELOW, lastViewID);
+
+                    // set click listener for the box
+                    new_listing_layout.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            expandListing(v, blockHeight*2);
+                        }
+                    });
 
                     lastViewID = View.generateViewId();
                     new_listing_layout.setId(lastViewID);
@@ -199,6 +207,58 @@ public class SavedListingsActivity extends MyActivity {
                 iv.setLayoutParams(lp);
 
                 return iv;
+            }
+
+            /**
+             * Expand the listing to newHeight and add / move buttons to correct positions
+             *
+             * @param v view being expanded
+             * @param newHeight new  height of view
+             */
+            protected void expandListing(View v, int newHeight) {
+                expand(v, newHeight);
+
+//                RelativeLayout.LayoutParams rlp = (RelativeLayout.LayoutParams) v.getLayoutParams();
+//                rlp.height = newHeight;
+//                v.setLayoutParams(rlp);
+
+
+            }
+
+            /**
+             * http://stackoverflow.com/questions/4946295/android-expand-collapse-animation
+             * @param v
+             */
+            public void expand(final View v, int newHeight) {
+
+//                v.measure(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+//                final int targetHeight = v.getMeasuredHeight()*2;
+
+                final int targetHeight = newHeight;
+                final int initialHeight = v.getLayoutParams().height;
+
+//                v.getLayoutParams().height = 0;
+//                v.setVisibility(View.VISIBLE);
+                Animation a = new Animation()
+                {
+                    @Override
+                    protected void applyTransformation(float interpolatedTime, Transformation t) {
+//                        v.getLayoutParams().height = interpolatedTime == 1
+//                                ? RelativeLayout.LayoutParams.WRAP_CONTENT
+//                                : (int)(targetHeight * interpolatedTime);
+                        v.getLayoutParams().height = (int) ((targetHeight - initialHeight) * interpolatedTime + initialHeight);
+                        v.requestLayout();
+                    }
+
+                    @Override
+                    public boolean willChangeBounds() {
+                        return true;
+                    }
+                };
+
+                // 1dp/ms
+                a.setDuration((int)(targetHeight / v.getContext().getResources().getDisplayMetrics().density));
+                v.startAnimation(a);
             }
 
         };
